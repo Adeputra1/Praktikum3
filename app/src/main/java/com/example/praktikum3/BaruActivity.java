f1b53da9deec806a594ff33417ad293039f996a2
adeputra1 <adeputera124@gmail.com>
2020-02-10 16:33:36 +0700

    Initial commit

---
+++ app/src/main/java/com/example/praktikum3/BaruActivity.java
@@ -0,0 +1,16 @@
+package com.example.praktikum3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BaruActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_baru);
+    }
+
+
+}
